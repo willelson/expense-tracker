d9c451d7e68682f2abbad08bed4fa27d22e6aaa0
Will Elson <william.elson@incoproip.com>
2020-05-16 11:03:11 +0100

    Start with empty initial state

--- src/context/GlobalState.tsx
+++ src/context/GlobalState.tsx
@@ -9,12 +9,7 @@ interface globalContextType {
 }
 
 const initialState = {
-  transactions: [
-    { id: 1, text: 'Flower', amount: -20 },
-    { id: 2, text: 'Salary', amount: 300 },
-    { id: 3, text: 'Book', amount: -10 },
-    { id: 4, text: 'Camera', amount: 150 }
-  ],
+  transactions: [],
   dispatch: () => {}
 };
 
